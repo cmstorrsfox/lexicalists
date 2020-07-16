797698e87325082bca6b73aecc35b191e8dc2be4
Chris Storrs-Fox <cmstorrsfox@gmail.com>
2020-07-16 12:45:08 +0100

    sass added (needs testing) and responsive title (needs work)

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
+import '../../custom.scss';
 import Container from 'react-bootstrap/Container';
 import Jumbotron from 'react-bootstrap/Jumbotron';
+
 
 // variables for DataMuse
 const corsAnywhere = 'https://cors-anywhere.herokuapp.com/';
@@ -41,8 +43,9 @@ class App extends React.Component {
     return (
       <Container fluid className="App">
         <Jumbotron fluid>
-          <h1 className="display-2 text-light bg-dark py-2">Welcome to the Collocation Station</h1>
+          <h1 className="title text-light bg-dark py-2">Welcome to the Collocation Station</h1>
           <p className="lead">Use this tool to look up simple noun or adjective collocations for common words.</p>
+          <p className="font-italic">Note for Dom: This is just a placeholder name!</p>
 
         </Jumbotron>
         <SearchBar onSearch={this.search} />
